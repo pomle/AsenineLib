de64c7194a30e6384453b43ba5d0f4ff478d8007
Pontus Persson <pom@spotify.com>
2012-12-03 13:36:46 +0000

    - Adds sendToClient() method to File

--- File.php
+++ File.php
@@ -27,8 +27,11 @@ class File implements iFile
 		$timeCreated = $timeModified = time();
 
 		$query = DB::prepareQuery("SELECT id FROM asenine_files WHERE hash = %s", $File->getHash());
-		if(!$File->fileID = (int)DB::queryAndFetchOne($query))
-		{
+
+		$File->fileID = (int)DB::queryAndFetchOne($query);
+
+		if (!$File->fileID) {
+
 			$query = DB::prepareQuery("INSERT INTO
 				asenine_files (
 					time_created,
@@ -64,17 +67,21 @@ class File implements iFile
 
 		try
 		{
-			if( empty($fromURL) )
-				throw New FileException('URL empty');
+			if (empty($fromURL)) {
+				throw New FileException('URL empty.');
+			}
 
-			if( !$toFile )
+			if (!$toFile) {
 				$toFile = tempnam(ASENINE_DIR_TEMP, 'AsenineDownload');
+			}
 
-			if( !$d = @fopen($toFile, 'w') )
+			if (!$d = @fopen($toFile, 'w')) {
 				throw New FileException(sprintf('Could not open destination "%s" for writing', $toFile));
+			}
 
-			if( !$s = @fopen($fromURL, 'r') )
+			if (!$s = @fopen($fromURL, 'r')) {
 				throw New FileException(sprintf('Could not open source "%s" for reading', $fromURL));
+			}
 
 			$bufferSize = 512 * 16;
 
@@ -82,8 +89,9 @@ class File implements iFile
 
 			$downloadBytes = 0;
 
-			while(($buffer = fgets($s, $bufferSize)) !== false)
+			while (($buffer = fgets($s, $bufferSize)) !== false) {
 				$downloadBytes += fputs($d, $buffer);
+			}
 
 			$downloadTime = microtime(true) - $t;
 
@@ -92,7 +100,9 @@ class File implements iFile
 
 
 			$name = basename($fromURL);
-			if( strpos($name, '%') !== false ) $name = urldecode($name); ### If URL contains % we assume it's URL encoded.
+			if (strpos($name, '%') !== false) {
+				$name = urldecode($name); ### If URL contains % we assume it's URL encoded.
+			}
 
 
 			$File = new static($toFile, filesize($toFile), null, $name);
@@ -106,8 +116,13 @@ class File implements iFile
 		}
 		catch(\Exception $e)
 		{
-			if( $d ) fclose($d);
-			if( $s ) fclose($s);
+			if($d) {
+				fclose($d);
+			}
+
+			if($s) {
+				fclose($s);
+			}
 
 			throw $e;
 		}
@@ -131,8 +146,9 @@ class File implements iFile
 
 	public static function loadFromDB(Archiver $Archiver, $fileIDs)
 	{
-		if( !$returnArray = is_array($fileIDs) )
+		if (!$returnArray = is_array($fileIDs)) {
 			$fileIDs = (array)$fileIDs;
+		}
 
 		$files = array_fill_keys($fileIDs, false);
 
@@ -152,8 +168,7 @@ class File implements iFile
 
 		$result = DB::fetch($query);
 
-		while($file = DB::assoc($result))
-		{
+		while ($file = DB::assoc($result)) {
 			$fileID = (int)$file['file_id'];
 
 			$fileLocation = $Archiver->getFileLocation($file['hash']);
@@ -190,8 +205,7 @@ class File implements iFile
 		$timeCreated = $timeModified = time();
 
 		### Try find the file on hash first, and set it's ID
-		if(!isset($File->fileID))
-		{
+		if (!isset($File->fileID)) {
 			self::createInDB($File);
 		}
 
@@ -214,8 +228,9 @@ class File implements iFile
 
 	public function __construct($location, $size = null, $mime = null, $name = null, $hash = null)
 	{
-		if( !is_string($location) )
+		if (!is_string($location)) {
 			trigger_error(__METHOD__ . ' expects arg #1 to be string, ' . gettype($location) . ' given', E_USER_WARNING);
+		}
 
 		$location = (string)$location;
 
@@ -228,8 +243,9 @@ class File implements iFile
 		$this->location = $location;
 
 		### File size can only be integer and must not be negative
-		if( !is_null($size) && ( !is_int($size) && ( $size < 0 ) ) )
+		if (!is_null($size) && (!is_int($size) && ($size < 0 ))) {
 			throw New FileException(sprintf("Size must be integer and 0 or more"));
+		}
 
 		$this->hash = $hash;
 		$this->size = $size ?: filesize($this->location);
@@ -271,8 +287,9 @@ class File implements iFile
 
 	public function copy($to)
 	{
-		if( !copy($this->location, $to) )
+		if (!copy($this->location, $to)) {
 			throw new FileException(sprintf('File copy from "%s" to "%s" failed', $this->location, $to));
+		}
 
 		$File_New = clone $this;
 		$File_New->location = $to;
@@ -282,8 +299,9 @@ class File implements iFile
 
 	public function delete()
 	{
-		if( !unlink($this->location) )
+		if (!unlink($this->location)) {
 			throw new FileException(sprintf('File delete from "%s" failed', $this->location));
+		}
 
 		return true;
 	}
@@ -295,8 +313,9 @@ class File implements iFile
 
 	public function link($at)
 	{
-		if( !symlink($this->location, $at) )
+		if (!symlink($this->location, $at)) {
 			throw new FileException(sprintf('File symlinking from "%s" to "%s" failed', $this->location, $at));
+		}
 
 		$File_Link = clone $this;
 		$File_Link->location = $at;
@@ -306,8 +325,9 @@ class File implements iFile
 
 	public function move($to)
 	{
-		if( !rename($this->location, $to) )
+		if (!rename($this->location, $to)) {
 			throw new FileException(sprintf('File move from "%s" to "%s" failed', $this->location, $to));
+		}
 
 		$this->location = $to;
 
@@ -321,14 +341,17 @@ class File implements iFile
 
 	public function getExtension()
 	{
-		if(!isset($this->extension))
-		{
-			if(($n = explode('.', $this->name)) && ($ext = end($n)))
+		if (!isset($this->extension)) {
+
+			if (($n = explode('.', $this->name)) && ($ext = end($n))) {
 				$this->extension = $ext;
-			elseif(($m = explode('/', $this->getMime())) && ($ext = end($m)))
+			}
+			elseif (($m = explode('/', $this->getMime())) && ($ext = end($m))) {
 				$this->extension = $ext;
-			else
+			}
+			else {
 				$this->extension = null;
+			}
 		}
 
 		return $this->extension;
@@ -336,8 +359,9 @@ class File implements iFile
 
 	public function getHash()
 	{
-		if( is_null($this->hash) )
+		if (is_null($this->hash)) {
 			$this->hash = hash_file('sha256', $this->location, false);
+		}
 
 		return $this->hash;
 	}
@@ -349,8 +373,7 @@ class File implements iFile
 
 	public function getMime()
 	{
-		if( is_null($this->mime) )
-		{
+		if (is_null($this->mime)) {
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
 			$this->mime = finfo_file($finfo, $this->location);
 			finfo_close($finfo);
@@ -366,8 +389,9 @@ class File implements iFile
 
 	public function getSize()
 	{
-		if( is_null($this->size) )
+		if (is_null($this->size)) {
 			$this->size = filesize($this->location);
+		}
 
 		return $this->size;
 	}
@@ -375,6 +399,40 @@ class File implements iFile
 	public function reads()
 	{
 		return is_readable($this->location);
+	}
+
+	public function sendToClient($name = null, $contentType = null)
+	{
+		if (!$this->exists()) {
+			throw new FileException('File does not exist on disk.');
+		}
+
+		if (!$this->reads()) {
+			throw new FileException('File is not readable.');
+		}
+
+		if (strlen($name) > 0) {
+			$fileName = $name;
+		}
+		else {
+			$fileName = $this->getName();
+		}
+
+		if (strlen($contentType) == 0) {
+			$contentType = $this->getMime();
+		}
+
+		ini_set('zlib.output_compression', 'off');
+		header('Accept-Ranges: bytes');
+		header('Content-Type: ' . $contentType);
+		header('Content-Disposition: attachment; filename="' . $fileName . '"');
+		header('Content-Length: ' . $this->getSize());
+
+		$res = @readfile($this->getLocation());
+
+		if (false === $res) {
+			throw new FileException('File send failed.');
+		}
 	}
 
 	public function writes()
