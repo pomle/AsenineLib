e3e02ecba145764d3c82b39d7d55e0b7c7b8bedd
Pontus Persson <pom@spotify.com>
2012-12-06 15:56:30 +0000

    Adds Password generation tool

--- Util/Token.php
+++ Util/Token.php
@@ -3,6 +3,26 @@ namespace Asenine\Util;
 
 class Token
 {
+	public static function createPassword($length = 12, $chars = 'abcdefghijkmnopqrstuvwxyz023456789!"#%/()$')
+	{
+		if (!is_int($length) || $length < 0) {
+			throw new \Exception('Argument #1 of ' . __METHOD__ . ' must be positive integer.');
+		}
+
+		if (!is_string($chars)) {
+			throw new \Exception('Argument #2 of ' . __METHOD__ . ' must be string.');
+		}
+
+		$chars = str_split($chars);
+		$password = '';
+
+		while ($length--) {
+			$password .= $chars[array_rand($chars)];
+		}
+
+		return $password;
+	}
+
 	public static function safeCompare($token1, $token2)
 	{
 		$salt = 'alx3jicalhm,qpictw9,cjom';
