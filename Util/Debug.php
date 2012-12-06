6d49eeb8da6086bb4e0b681db95af115e57e820c
Pontus Persson <pom@spotify.com>
2012-12-06 20:22:19 +0000

    Adds Debug util

---
+++ Util/Debug.php
@@ -0,0 +1,31 @@
+<?
+/**
+ * Debug class containing various functions for simplifying debug output and logging.
+ *
+ * @author Pontus Persson <pom@spotify.com>
+ */
+namespace Asenine\Util;
+
+class Debug
+{
+	/**
+	 * Returns debug string if in debug mode, otherwise production string is
+	 *	returned and debug string outputted as error for log grabbing.
+	 *
+	 * @param string $debug 		String to output if debug mode is on.
+	 * @param string $production 	String to output if debug mode is off.
+	 * @return string
+	 */
+	public static function log($debug, $production)
+	{
+		$isDebugging = (defined('DEBUG') && (true === constant('DEBUG')));
+
+		if (true === $isDebugging) {
+			return $debug;
+		}
+		else {
+			trigger_error($debug, E_USER_NOTICE);
+			return $production;
+		}
+	}
+}
