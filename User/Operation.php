b384c84b1fb636105318d79cc1779509d94e99ef
Pontus Persson <pom@spotify.com>
2012-12-07 10:48:46 +0000

    Improves code standard Throws UserException instead of \Exception

--- User/Operation.php
+++ User/Operation.php
@@ -12,19 +12,22 @@ class Operation
 	{
 		self::verifyPassword($newPassword, $newPasswordVerify);
 
-		if(User::verifyPassword($userID, $currentPassword) !== true)
+		if (true !== User::verifyPassword($userID, $currentPassword)) {
 			throw new UserException(_('Current password does not match.'));
+		}
 
 		return Manager::setPassword($userID, $newPassword);
 	}
 
 	public static function verifyPassword($newPassword, $newPasswordVerify)
 	{
-		if( strlen($newPassword) < User::PASSWORD_MIN_LEN)
-			throw new \Exception(sprintf(_('Password invalid. Must consist of at least %d characters.'), User::PASSWORD_MIN_LEN));
+		if (User::PASSWORD_MIN_LEN > strlen($newPassword)) {
+			throw new UserException(sprintf(_('Password invalid. Must consist of at least %d characters.'), User::PASSWORD_MIN_LEN));
+		}
 
-		if( $newPassword !== $newPasswordVerify )
-			throw new \Exception(_('New password mismatch.'));
+		if ($newPassword !== $newPasswordVerify) {
+			throw new UserException(_('New password mismatch.'));
+		}
 	}
 
 	public static function verifyUsername($username, $discountUserID = 0)
@@ -33,11 +36,14 @@ class Operation
 		$minLen = User::USERNAME_MIN_LEN;
 		$maxLen = User::USERNAME_MAX_LEN;
 
-		if( (isset($minLen) && $usernameLen < $minLen) || (isset($maxLen) && $usernameLen > $maxLen) )
-			throw New \Exception(sprintf(_('Username lenght invalid. Must consist of between %u and %u characters.'), $minLen, $maxLen));
+		if ((isset($minLen) && $usernameLen < $minLen) || (isset($maxLen) && $usernameLen > $maxLen)) {
+			throw new UserException(sprintf(_('Username lenght invalid. Must consist of between %u and %u characters.'), $minLen, $maxLen));
+		}
 
-		$query = DB::prepareQuery("SELECT COUNT(*) FROM asenine_users WHERE username = %s AND NOT ID = %u", $username, $discountUserID);
-		if( (bool)DB::queryAndFetchOne($query) )
-			throw New \Exception(sprintf(_('The username "%s" is already taken'), $username));
+		$query = DB::prepareQuery("SELECT id FROM asenine_users WHERE username = %s AND NOT id = %u", $username, $discountUserID);
+
+		if ((bool)DB::queryAndFetchOne($query)) {
+			throw new UserException(sprintf(_('The username "%s" is already taken'), $username));
+		}
 	}
 }
