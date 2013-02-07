e7a64ae959393e02139f76205abe2e4e8ef9471e
Pontus Persson <pom@spotify.com>
2013-02-07 12:08:30 +0000

    Add ForceDownload class

---
+++ Util/ForceDownload.php
@@ -0,0 +1,37 @@
+<?
+namespace Asenine\Util;
+
+class ForceDownload
+{
+	protected $data = '';
+
+	public function __construct()
+	{
+	}
+
+
+	public function addData($string)
+	{
+		$this->data .= $string;
+		return $this;
+	}
+
+	public function addFile($filename)
+	{
+		$this->addData(file_get_contents($filename));
+		return $this;
+	}
+
+	public function send($name, $contentType = 'application/octet-stream')
+	{
+		ini_set('zlib.output_compression', 'off');
+
+		header('Expires: ' . date('r', time() + 60*60*24*30));
+		header('Accept-Ranges: bytes');
+		header('Content-Type: ' . $contentType);
+		header('Content-Disposition: attachment; filename="' . $name . '"');
+		header('Content-Length: ' . strlen($this->data));
+
+		echo $this->data;
+	}
+}
