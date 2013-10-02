7685fd2c252311e6dc9a3bf85632b18dff786a9d
Pontus Persson <pom@spotify.com>
2013-10-02 12:52:56 +0000

    Add Date Sanitize Util

---
+++ lib/Util/Sanitize/Date.php
@@ -0,0 +1,34 @@
+<?
+/**
+ * Class containing methods for sanitizing date strings and throwing exceptions.
+ */
+namespace Asenine\Util\Sanitize;
+
+class Date
+{
+	public function __construct($desiredFormat = 'Y-m-d')
+	{
+		$this->toFormat = $desiredFormat;
+	}
+
+	public function orExcept($input)
+	{
+		try {
+			$D = new \DateTime($input);
+			return $D->format($this->toFormat);
+		}
+		catch (\Exception $e) {
+			throw new \InvalidArgumentException('Date erroneously formatted');
+		}
+	}
+
+	public function orFallback($input, $fallback = null)
+	{
+		try {
+			return $this->orExcept($input);
+		}
+		catch (\Exception $e) {
+			return $fallback;
+		}
+	}
+}
